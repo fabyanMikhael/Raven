bcc621ec1aaa8ce9dac42d0b4833e413694e9844
Fabyan Mikhael <fabyan.mikhael@ryerson.ca>
2022-05-12 12:41:13 -0400

    testing interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 #![allow(non_snake_case)]
-use interpreter::interpreter::ParseFile;
+use std::{fs::read_to_string, cell::RefCell, rc::Rc};
 
-pub mod interpreter;
+use interpreter::interpreter::Interpreter;
+use parser::parser::{ParseFile, Type};
+
+pub mod parser;
 mod tests;
+pub mod vm;
+pub mod interpreter;
 
 fn main() {
-    println!("{:?}",ParseFile("test.rv"));
+    let mut i = Interpreter::new();
+    i.addFunction("print", |_, args|{
+        let text = args.into_iter().map(|e| e.borrow().toString()).collect::<Vec<_>>().join(" ");
+        println!("{}", text);
+        None
+    });
+    i.addFunction("number", |_, args|{
+        Some(Type::Number(Interpreter::String(args[0].clone()).parse::<f32>().unwrap()).into())
+    });
+    i.run(read_to_string("src/tests/scripts/basics.rv").unwrap());
 }
